7c0a3f530fc2cbaaec42766437066a6f16c22dfe
Amaury <iamaury1380@gmail.com>
2022-10-17 13:48:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ typedef struct{
     double tottour;
 } voiture;
 
-double generer_temps(int lower, int upper){
+double generateTime(int lower, int upper){
     srand(clock());
     return (rand() % (upper - lower + 1)) + lower;
 }
@@ -20,18 +20,18 @@ int main() {
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 20; j++){
             v[j].tottour = 99999;
-            v[j].s1 = generer_temps(25000, 45000) / 1000;
+            v[j].s1 = generateTime(25000, 45000) / 1000;
             best1 = v[j].s1;
-            v[j].s2 = generer_temps(25000, 45000) / 1000;
+            v[j].s2 = generateTime(25000, 45000) / 1000;
             best2 = v[j].s2;
-            v[j].s3 = generer_temps(25000, 45000) / 1000;
+            v[j].s3 = generateTime(25000, 45000) / 1000;
             best3 = v[j].s3;
             if (v[j].s1 + v[j].s2 + v[j].s3 < v[j].tottour){ v[j].tottour = v[j].s1 + v[j].s2 + v[j].s3; };        
         }
     }
     
 
-    printf("==================================================================\nID\tS1\t\tS2\t\tS3\t\tTOT\n\n");
+    printf("==================================================================\nID\tBEST S1\t\tBEST S2\t\tBEST S3\t\tBEST TOTAL\n\n");
     for(int k = 0; k < 20; k++) {
         printf("%d\t%f\t%f\t%f\t%f\n", v[k].id, v[k].s1, v[k].s2, v[k].s3, v[k].tottour);
     }
