66557b208b19e2032e6f9c2fd03ee398dde1b93d
Amaury <iamaury1380@gmail.com>
2022-10-17 14:43:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 typedef struct{
     int id;
@@ -10,12 +11,25 @@ typedef struct{
     double tottour;
 } voiture;
 
+voiture v[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
+
 double generateTime(int lower, int upper){
     srand(clock());
     return (rand() % (upper - lower + 1)) + lower;
 }
+
+char formatTime(double time) {
+    char buffer[8];
+    int minutes = time / 60;
+    int seconds = time / 1;
+    int milliseconds = time / 0.001;
+    
+    sprintf(buffer, "%i:%i.%i", minutes, seconds, milliseconds);
+
+    return *buffer;
+}
+
 int main() {
-    voiture v[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
     double best1, best2, best3 = 99999;
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 20; j++){
@@ -36,4 +50,7 @@ int main() {
         printf("%d\t%f\t%f\t%f\t%f\n", v[k].id, v[k].s1, v[k].s2, v[k].s3, v[k].tottour);
     }
     printf("==================================================================\n");
+
+    char form = formatTime(568.15);
+    printf("%i", form);
 }
