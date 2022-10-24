0e6f367e7d5bb5b8f464ee7beba51b6325d9c4b4
Amaury <iamaury1380@gmail.com>
2022-10-24 14:44:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,54 +3,62 @@
 #include <time.h>
 #include <math.h>
 
+#define MIN_TIME = 25000
+#define MAX_TIME = 45000
+
 typedef struct{
     int id;
     double s1;
     double s2;
     double s3;
-    double tottour;
-} voiture;
+    double tt;
+} Voiture;
 
-voiture v[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
+Voiture v[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
 
 double generateTime(int lower, int upper){
+    // Génère un temps aléatoire entre 'lower' et 'upper'
     srand(clock());
     return (rand() % (upper - lower + 1)) + lower;
 }
 
-char formatTime(double time) {
-    char buffer[8];
-    int minutes = time / 60;
-    int seconds = time / 1;
-    int milliseconds = time / 0.001;
-    
-    sprintf(buffer, "%i:%i.%i", minutes, seconds, milliseconds);
+void bestTime(char type[2]){
+    print('%s', v[0][type]);
+}
 
-    return *buffer;
+//char formatTime(char * test[], double time) {
+//    char buffer[8];
+//    int minutes = time / 60;
+//    int seconds = time / 1;
+//    int milliseconds = time / 0.001;
+//
+//    sprintf(*test, "%i:%i.%i", minutes, seconds, milliseconds);
+//
+//    return test;
+//}
+
+void turn() {
+    for (int j = 0; j < 20; j++) { // Voitures
+        v[j].tt = 99999;
+        v[j].s1 = generateTime(MIN_TIME, MAX_TIME) / 1000;
+        v[j].s2 = generateTime(MIN_TIME, MAX_TIME) / 1000;
+        v[j].s3 = generateTime(MIN_TIME, MAX_TIME) / 1000;
+        if (v[j].s1 + v[j].s2 + v[j].s3 < v[j].tt) {
+            v[j].tt = v[j].s1 + v[j].s2 + v[j].s3;
+        };
+    }
 }
 
 int main() {
-    double best1, best2, best3 = 99999;
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 20; j++){
-            v[j].tottour = 99999;
-            v[j].s1 = generateTime(25000, 45000) / 1000;
-            best1 = v[j].s1;
-            v[j].s2 = generateTime(25000, 45000) / 1000;
-            best2 = v[j].s2;
-            v[j].s3 = generateTime(25000, 45000) / 1000;
-            best3 = v[j].s3;
-            if (v[j].s1 + v[j].s2 + v[j].s3 < v[j].tottour){ v[j].tottour = v[j].s1 + v[j].s2 + v[j].s3; };        
-        }
-    }
+//    for (int i = 0; i < 10; i++) { // Tours
+        turn();
+//    }
+    bestTime('s1');
     
 
-    printf("==================================================================\nID\tBEST S1\t\tBEST S2\t\tBEST S3\t\tBEST TOTAL\n\n");
+    printf("=======================================\nID\tS1\tS2\tS3\tTT\n\n");
     for(int k = 0; k < 20; k++) {
-        printf("%d\t%f\t%f\t%f\t%f\n", v[k].id, v[k].s1, v[k].s2, v[k].s3, v[k].tottour);
+        printf("%d\t%.3f\t%.3f\t%.3f\t%.3f\n", v[k].id, v[k].s1, v[k].s2, v[k].s3, v[k].tt);
     }
-    printf("==================================================================\n");
-
-    char form = formatTime(568.15);
-    printf("%i", form);
+    printf("=======================================\n");
 }
