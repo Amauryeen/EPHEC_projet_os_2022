74642a7789bfcd6a2d9de8cc50242ab4fbeb98b0
Amaury <iamaury1380@gmail.com>
2022-11-07 14:15:26 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,28 +3,13 @@
 #include <time.h>
 #include <math.h>
 
-#define MIN_TIME = 25000
-#define MAX_TIME = 45000
+#include "lib/voitures.h"
+#include "lib/genererTemps.h"
+#include "lib/tournerVoitures.h"
 
-typedef struct{
-    int id;
-    double s1;
-    double s2;
-    double s3;
-    double tt;
-} Voiture;
-
-Voiture v[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
-
-double generateTime(int lower, int upper){
-    // Génère un temps aléatoire entre 'lower' et 'upper'
-    srand(clock());
-    return (rand() % (upper - lower + 1)) + lower;
-}
-
-void bestTime(char type[2]){
-    print('%s', v[0][type]);
-}
+//void bestTime(char type[2]){
+//    printf('%s', v[0]);
+//}
 
 //char formatTime(char * test[], double time) {
 //    char buffer[8];
@@ -37,28 +22,6 @@ void bestTime(char type[2]){
 //    return test;
 //}
 
-void turn() {
-    for (int j = 0; j < 20; j++) { // Voitures
-        v[j].tt = 99999;
-        v[j].s1 = generateTime(MIN_TIME, MAX_TIME) / 1000;
-        v[j].s2 = generateTime(MIN_TIME, MAX_TIME) / 1000;
-        v[j].s3 = generateTime(MIN_TIME, MAX_TIME) / 1000;
-        if (v[j].s1 + v[j].s2 + v[j].s3 < v[j].tt) {
-            v[j].tt = v[j].s1 + v[j].s2 + v[j].s3;
-        };
-    }
-}
-
 int main() {
-//    for (int i = 0; i < 10; i++) { // Tours
-        turn();
-//    }
-    bestTime('s1');
-    
-
-    printf("=======================================\nID\tS1\tS2\tS3\tTT\n\n");
-    for(int k = 0; k < 20; k++) {
-        printf("%d\t%.3f\t%.3f\t%.3f\t%.3f\n", v[k].id, v[k].s1, v[k].s2, v[k].s3, v[k].tt);
-    }
-    printf("=======================================\n");
+    #include "lib/main.h"
 }
