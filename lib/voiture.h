4ec0c3ef559d75d2550ef0b0ab0089d4fd688cf7
Amaury <iamaury1380@gmail.com>
2022-11-07 14:06:33 +0100

    Create voiture.h

---
+++ lib/voiture.h
@@ -0,0 +1,9 @@
+typedef struct{
+    int id;
+    double s1;
+    double s2;
+    double s3;
+    double tt;
+} Voiture;
+
+Voiture v[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
