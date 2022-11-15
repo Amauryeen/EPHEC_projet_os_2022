08b1a7a79fbcdbac513a05a0406f720e09338005
Amaury <iamaury1380@gmail.com>
2022-11-15 14:39:11 +0100

    Add files via upload

--- lib/tournerVoitures.h
+++ lib/tournerVoitures.h
@@ -3,12 +3,12 @@
 
 void turn() {
     for (int j = 0; j < 20; j++) { // Voitures
-        v[j].tt = 99999;
-        v[j].s1 = generateTime(MIN_TIME, MAX_TIME) / 1000;
-        v[j].s2 = generateTime(MIN_TIME, MAX_TIME) / 1000;
-        v[j].s3 = generateTime(MIN_TIME, MAX_TIME) / 1000;
-        if (v[j].s1 + v[j].s2 + v[j].s3 < v[j].tt) {
-            v[j].tt = v[j].s1 + v[j].s2 + v[j].s3;
+        voitures[j].tt = 99999;
+        voitures[j].s1 = generateTime(MIN_TIME, MAX_TIME) / 1000;
+        voitures[j].s2 = generateTime(MIN_TIME, MAX_TIME) / 1000;
+        voitures[j].s3 = generateTime(MIN_TIME, MAX_TIME) / 1000;
+        if (voitures[j].s1 + voitures[j].s2 + voitures[j].s3 < voitures[j].tt) {
+            voitures[j].tt = voitures[j].s1 + voitures[j].s2 + voitures[j].s3;
         };
     }
 }
--- main.c
+++ main.c
@@ -6,64 +6,45 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-#include "lib/voitures.h"
-#include "lib/genererTemps.h"
-#include "lib/tournerVoitures.h"
-
-//void bestTime(char type[2]){
-//    printf('%s', v[0]);
-//}
-
-//char formatTime(char * test[], double time) {
-//    char buffer[8];
-//    int minutes = time / 60;
-//    int seconds = time / 1;
-//    int milliseconds = time / 0.001;
-//
-//    sprintf(*test, "%i:%i.%i", minutes, seconds, milliseconds);
-//
-//    return test;
-//}
+// #include "lib/voitures.h"
+// #include "lib/genererTemps.h"
+// #include "lib/tournerVoitures.h"
 
 int main() {
-    for (int i = 0; i < 10; i++) { // Tours
-        turn();
-    }
+    // for (int i = 0; i < 10; i++) { // Tours
+    //     turn();
+    // }
     
 
-    printf("=======================================\nID\tS1\tS2\tS3\tTT\n\n");
-    for(int k = 0; k < 20; k++) {
-        printf("%d\t%.3f\t%.3f\t%.3f\t%.3f\n", v[k].id, v[k].s1, v[k].s2, v[k].s3, v[k].tt);
-    }
-    printf("=======================================\n");
+    // printf("=======================================\nID\tS1\tS2\tS3\tTT\n\n");
+    // for(int k = 0; k < 20; k++) {
+    //     printf("%d\t%.3f\t%.3f\t%.3f\t%.3f\n", voitures[k].id, voitures[k].s1, voitures[k].s2, voitures[k].s3, voitures[k].tt);
+    // }
+    // printf("=======================================\n");
     
-    // mémoire partagée
-    int shmid = shmget(1000, 20 * sizeof(Voiture), 0666 | IPC_CREAT);
-    if (shmid == -1) {
-            printf("shmget() failed !");
-    }
+    // // mémoire partagée
+    // int shmid = shmget(1000, 20 * sizeof(Voiture), 0666 | IPC_CREAT);
+    // if (shmid == -1) {
+    //         printf("shmget() failed !");
+    // }
     
-    struct Voiture *circuit;
-    circuit = (void *) shmat(shmid,0,0);
-    //circuit = (struct voiture *) shmat(shmid,0,0);
-    if (circuit == -1) {
-            printf("shmat() failed !");
-    }
+    // struct Voiture *circuit;
+    // //circuit = (void *) shmat(shmid,0,0);
+    // circuit = (struct Voiture *) shmat(shmid, 0, 0);
+    // if (circuit == NULL) {
+    //         printf("shmat() failed !");
+    // }
     
-    int pid;
-    for (int i = 0; i < 20; i++){
-        pid = fork();
+    // int pid;
+    for (int i = 0; i < 20; i++) {
 
-        if (pid == -1){ // tester s'il y a une erreur dans le fork
-            printf("Error in fork()");
+        // if(pid == -1) { // tester s'il y a une erreur dans le fork
+        //     printf("Error in fork()");
+        // }
+
+        if(fork() == 0) { // Fils
+            printf("Fils %d\n", i);
+            exit(0);
         }
-        if (pid ==0){ // exécution du fils
-
-        }
-        if (pid > 0){ // exécution du père
-
-        }
-
-
     }
 }
