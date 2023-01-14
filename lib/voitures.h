11d537c3c36e81b163938d0719fcb6f8d08a9d20
Amaury <iamaury1380@gmail.com>
2023-01-14 14:05:53 +0100

    Delete voitures.h

--- lib/voitures.h
+++
@@ -1,9 +0,0 @@
-typedef struct{
-    int id;
-    double s1;
-    double s2;
-    double s3;
-    double tt;
-} Voiture;
-
-Voiture voitures[20] = {{44}, {63}, {1}, {11}, {55}, {16}, {4}, {3}, {14}, {31}, {10}, {22}, {5}, {18}, {6}, {23}, {77}, {24}, {47}, {9}};
